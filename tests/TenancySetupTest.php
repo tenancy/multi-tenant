c4e137cb5c41ab451751545824b0048c4850b670
Daniel Klabbers <dklabbers@hostingxs.nl>
2015-06-17 20:00:48 +0200

    first test

--- tests/TenancySetupTest.php
+++ tests/TenancySetupTest.php
@@ -1,2 +1,14 @@
 <?php namespace HynMe\MultiTenant\Tests;
 
+use App;
+use TestCase;
+
+class TenancySetupTest extends TestCase
+{
+
+    public function testSetupHasNotRun()
+    {
+        $this->tenant = App::make('tenant.hostname');
+        $this->assertNull($this->tenant);
+    }
+}
