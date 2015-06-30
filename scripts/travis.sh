81e5ae2302aa59dd9103771fd88d7971ee1e0ac4
Daniel Klabbers <dklabbers@hostingxs.nl>
2015-06-30 11:02:33 +0200

    travis retry fails the build on bash

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -9,11 +9,11 @@ cd ${TRAVIS_BUILD_DIR}/laravel
 echo "requiring composer package ${TRAVIS_REPO_SLUG} ${TRAVIS_BRANCH}"
 
 if [[ ${TRAVIS_BRANCH} =~ "^(([0-9]+\.)+[0-9]+)$" ]]; then
-    travis_retry composer require ${TRAVIS_REPO_SLUG}:${TRAVIS_BRANCH}
+    composer require ${TRAVIS_REPO_SLUG}:${TRAVIS_BRANCH}
 else
     # development package of framework could be required for the package
-    travis_retry composer require hyn-me/framework "dev-master as 0.1.99"
-    travis_retry composer require ${TRAVIS_REPO_SLUG}:dev-${TRAVIS_BRANCH}
+    composer require hyn-me/framework "dev-master as 0.1.99"
+    composer require ${TRAVIS_REPO_SLUG}:dev-${TRAVIS_BRANCH}
 fi
 
 # moves the unit test to the root laravel directory
