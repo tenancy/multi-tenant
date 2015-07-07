892c54091b373d1ebfd3b5b8bc5acba02a809382
Daniel Klabbers <daniel+hyn@klabbers.email>
2015-07-07 22:07:34 +0200

    moved registration of migration commands backwards

--- src/HynMe/MultiTenant/MultiTenantServiceProvider.php
+++ src/HynMe/MultiTenant/MultiTenantServiceProvider.php
@@ -45,6 +45,12 @@ class MultiTenantServiceProvider extends ServiceProvider {
          * Model observers
          */
         $this->observers();
+        
+        /*
+         * override the default migrate command
+         */
+        $this->registerCommands();
+
     }
 
     /**
@@ -64,11 +70,6 @@ class MultiTenantServiceProvider extends ServiceProvider {
 	 */
 	public function register()
 	{
-        /*
-         * override the default migrate command
-         */
-        $this->registerCommands();
-
         /*
          * Bind setup command into ioc
          */
