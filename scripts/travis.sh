f3ed833f4b119ffa0612b956e23a3d87b97ee8ce
Daniel Klabbers <daniel+hyn@klabbers.email>
2015-07-23 21:55:08 +0200

    move of phpunit is required

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -13,9 +13,10 @@ if [[ ${TRAVIS_BRANCH} =~ $BRANCH_REGEX ]]; then
     composer require ${TRAVIS_REPO_SLUG}:${TRAVIS_BRANCH}
 else
     echo "composer require ${TRAVIS_REPO_SLUG}:dev-${TRAVIS_BRANCH}"
-    # development package of framework could be required for the package
     composer require "${TRAVIS_REPO_SLUG}:dev-${TRAVIS_BRANCH}#${TRAVIS_COMMIT}"
 fi
 
-#phpunit
-phpunit --coverage-text --coverage-clover=${TRAVIS_BUILD_DIR}/coverage.clover --configuration ./vendor/${TRAVIS_REPO_SLUG}/phpunit.travis.xml
+# moves the unit test to the root laravel directory
+cp ./vendor/${TRAVIS_REPO_SLUG}/phpunit.travis.xml ./phpunit.xml
+
+phpunit --coverage-text --coverage-clover=${TRAVIS_BUILD_DIR}/coverage.clover --configuration
