97410e7f7d801b65d7be7e5657e02cb69df7957c
Daniël Klabbers <daniel.klabbers@aboutyou.de>
2016-06-08 23:32:16 +0200

    removed unneeded todo in phpdoc

--- database/migrations/2015_05_19_000001_hws_ssl_hostnames_table.php
+++ database/migrations/2015_05_19_000001_hws_ssl_hostnames_table.php
@@ -1,8 +1,8 @@
 <?php
 
+use Hyn\MultiTenant\Tenant\DatabaseConnection;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Hyn\MultiTenant\Tenant\DatabaseConnection;
 
 class HwsSslHostnamesTable extends Migration
 {
@@ -33,9 +33,6 @@ class HwsSslHostnamesTable extends Migration
             $table->index('ssl_certificate_id');
             // relations
             $table->foreign('ssl_certificate_id')->references('id')->on('ssl_certificates')->onDelete('cascade');
-            // the set null constraint does not work on mariadb; let's just ignore that for now
-            // @TODO but fix support for set null in the future somehow
-//            $table->foreign('hostname_id')->references('id')->on('hostnames')->onDelete('set null');
         });
     }
 
