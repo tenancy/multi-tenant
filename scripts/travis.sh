ab9d127b7e52c771457323453e2a34c6dc813144
Daniël Klabbers <daniel.klabbers@aboutyou.de>
2016-06-09 06:12:16 +0200

    attempt at running beanstalkd for further testing of writing and changing webserver configurations

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -4,6 +4,25 @@
 # v prints every line before executing
 set -ev
 
+#
+# Set up supervisor and the beanstalk queue
+#
+sudo cat <<EOF > /etc/supervisor/conf.d/laravel-queue.conf
+[program:travis-queue]
+command=php artisan queue:work default --env=testing --daemon
+process_name=travis_queue
+numprocs=1
+autostart=1
+autorestart=1
+user=root
+directory=$TRAVIS_BUILD_DIR/vendor/laravel/laravel
+stdout_logfile=/var/log/queue.log
+redirect_stderr=true
+EOF
+
+sudo supervisorctl reread
+sudo supervisorctl update
+
 # moves the unit test to the root laravel directory
 cp phpunit.travis.xml phpunit.xml
 
