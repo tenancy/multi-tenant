fa11fda4d1f0dccdbbea82a85defc4b34aa64785
Daniel Klabbers <daniel@productip.com>
2016-12-20 14:30:28 +0100

    wrong path for configuration file

--- src/Providers/Tenants/ConfigurationProvider.php
+++ src/Providers/Tenants/ConfigurationProvider.php
@@ -9,11 +9,11 @@ class ConfigurationProvider extends ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom(
-            __DIR__ . '/../../assets/configs/tenancy.php',
+            __DIR__ . '/../../../assets/configs/tenancy.php',
             'tenancy'
         );
         $this->publishes([
-            __DIR__ . '/../../assets/configs/tenancy.php' => config_path('tenancy.php')
+            __DIR__ . '/../../../assets/configs/tenancy.php' => config_path('tenancy.php')
         ], 'tenancy');
     }
 }
