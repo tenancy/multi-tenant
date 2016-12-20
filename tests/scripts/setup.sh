815fa55414623fc997f418a2d2849c32d49f4235
Daniel Klabbers <daniel@productip.com>
2016-12-20 10:53:57 +0100

    let's drop queue driver beanstalk for now

--- tests/scripts/setup.sh
+++ tests/scripts/setup.sh
@@ -11,31 +11,6 @@ fi
 # set symlink so it seems as if this is a factual laravel installation
 ln -s $CI_PROJECT_DIR/vendor/ $CI_PROJECT_DIR/vendor/laravel/laravel/vendor
 
-# Set up supervisor and the beanstalk queue
-
-cat /etc/default/beanstalkd | sed -e "s/#START=yes/START=yes/" > /tmp/beanstalkd
-mv -f /tmp/beanstalkd /etc/default/beanstalkd
-
-cat <<EOF > /tmp/supervisor
-[program:travis-queue]
-command=php artisan queue:work default --env=testing --daemon
-process_name=travis_queue
-numprocs=1
-autostart=1
-autorestart=1
-user=root
-directory=$CI_PROJECT_DIR/vendor/laravel/laravel
-stdout_logfile=/var/log/travis-queue.log
-redirect_stderr=true
-EOF
-mv -f /tmp/supervisor /etc/supervisor/conf.d/laravel-queue.conf
-
-service beanstalkd start
-$BINDIR/supervisord
-
-supervisorctl reread
-supervisorctl update
-
 # moves the unit test to the root laravel directory
 cp ci.travis.xml phpunit.xml
 
