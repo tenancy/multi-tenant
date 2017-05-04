c37a5b72c764434db7f4bf496f83b7d0f400bb3b
Daniel Klabbers <daniel@productip.com>
2017-05-04 16:13:55 +0200

    migrate command only runs the exact path or realpath provided

--- src/Database/Console/MigrateCommand.php
+++ src/Database/Console/MigrateCommand.php
@@ -26,13 +26,22 @@ class MigrateCommand extends BaseCommand
      */
     protected function getMigrationPaths()
     {
-        $paths = parent::getMigrationPaths();
-
-        if (! is_null($realPath = $this->input->getOption('realpath'))) {
-            $paths[] = $realPath;
+        // Here, we will check to see if a path option has been defined. If it has we will
+        // use the path relative to the root of the installation folder so our database
+        // migrations may be run for any customized path from within the application.
+        if ($this->input->hasOption('path') && $this->option('path')) {
+            return collect($this->option('path'))->map(function ($path) {
+                return $this->laravel->basePath().'/'.$path;
+            })->all();
         }
 
-        return $paths;
+        if (! is_null($realPath = $this->input->getOption('realpath'))) {
+            return [$realPath];
+        }
+
+        return array_merge(
+            [$this->getMigrationPath()], $this->migrator->paths()
+        );
     }
 
     /**
