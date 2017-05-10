109e80bbd0801173b1447d4941faf89ae8e8d0d4
Daniel Klabbers <daniel@productip.com>
2017-05-10 11:18:50 +0200

    attempt to get pgsql up while mariadb does not

--- tests/scripts/setup.sh
+++ tests/scripts/setup.sh
@@ -19,4 +19,12 @@ if [ -f "$CI_PROJECT_DIR/vendor/laravel/laravel/config/webserver.php" ]; then
     rm "$CI_PROJECT_DIR/vendor/laravel/laravel/config/webserver.php"
 fi
 
+# We need to configure pdo for the specific connection.
+if [[ "$CI_JOB_NAME" == *-pgsql ]]; then
+    docker-php-ext-install pdo_pgsql
+fi
+if [[ "$CI_JOB_NAME" == *-mysql ]]; then
+    docker-php-ext-install pdo_mysql
+fi
+
 cd $CI_PROJECT_DIR
