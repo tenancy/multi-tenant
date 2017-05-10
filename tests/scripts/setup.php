2bdc217518c2e7b8bbc4b27cb42d701fad7cf50d
Daniël Klabbers <Luceos@users.noreply.github.com>
2017-05-10 16:32:32 +0200

    Apply fixes from StyleCI (#200)

--- tests/scripts/setup.php
+++ tests/scripts/setup.php
@@ -1,5 +1,17 @@
 <?php
 
+/*
+ * This file is part of the hyn/multi-tenant package.
+ *
+ * (c) Daniël Klabbers <daniel@klabbers.email>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see https://github.com/hyn/multi-tenant
+ *
+ */
+
 chdir(getenv('CI_PROJECT_DIR'));
 
 $base_path = __DIR__ . '/../../';
@@ -43,4 +55,3 @@ EOM;
         }
     }
 }
-
--- tests/unit-tests/Webserver/Vhost/ApacheGeneratorTest.php
+++ tests/unit-tests/Webserver/Vhost/ApacheGeneratorTest.php
@@ -1,5 +1,17 @@
 <?php
 
+/*
+ * This file is part of the hyn/multi-tenant package.
+ *
+ * (c) Daniël Klabbers <daniel@klabbers.email>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see https://github.com/hyn/multi-tenant
+ *
+ */
+
 namespace Hyn\Tenancy\Tests\Webserver\Vhost;
 
 use Hyn\Tenancy\Tests\Test;
@@ -16,6 +28,5 @@ class ApacheGeneratorTest extends Test
      */
     public function generates_vhost_configuration()
     {
-
     }
 }
