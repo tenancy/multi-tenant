a60e9634aaaf37b19584e780a28651d869f80ca1
Daniël Klabbers <luceos@users.noreply.github.com>
2017-07-11 16:57:17 +0200

    Apply fixes from StyleCI (#227)

--- src/Contracts/Generator/SavesToPath.php
+++ src/Contracts/Generator/SavesToPath.php
@@ -8,9 +8,8 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
+ * @see https://laravel-tenancy.com
  * @see https://github.com/hyn/multi-tenant
- * @see https://hyn.me
- * @see https://patreon.com/tenancy
  */
 
 namespace Hyn\Tenancy\Contracts\Generator;
