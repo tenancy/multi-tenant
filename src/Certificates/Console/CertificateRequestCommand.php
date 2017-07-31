8167c575882b93abb6311d51db9a53f1c3db6ca6
Daniel Klabbers <daniel@productip.com>
2017-07-31 16:44:06 +0200

    writing command, might need to look at acmephp command first

--- src/Certificates/Console/CertificateRequestCommand.php
+++ src/Certificates/Console/CertificateRequestCommand.php
@@ -3,16 +3,31 @@
 namespace Hyn\Tenancy\Certificates\Console;
 
 use AcmePhp\Core\AcmeClient;
+use AcmePhp\Core\Exception\Server\MalformedServerException;
+use Hyn\Tenancy\Contracts\Repositories\WebsiteRepository;
 use Illuminate\Console\Command;
 
 class CertificateRequestCommand extends Command
 {
-    protected $signature = 'tenancy:certificate {tenant}';
+    protected $signature = 'tenancy:certificate {website}';
 
     protected $description = 'Requests new Lets Encrypt certificate for tenant';
 
-    public function handle(AcmeClient $acme)
+    public function handle(AcmeClient $acme, WebsiteRepository $websites)
     {
+        $website = $this->argument('website');
+
+        $website = $websites->findByUuid($website);
+
+        if (!$website) {
+            throw new \InvalidArgumentException("Invalid website");
+        }
+
+        try {
+            $data = $acme->registerAccount();
+        } catch (MalformedServerException $e) {
+        }
+
 
     }
 }
