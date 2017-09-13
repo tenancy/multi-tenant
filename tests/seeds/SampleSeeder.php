d57d2b3e4dc3f9264d200411d817f66a69b86911
Daniël Klabbers <luceos@users.noreply.github.com>
2017-09-13 17:11:38 +0200

    Apply fixes from StyleCI (#249)

--- tests/seeds/SampleSeeder.php
+++ tests/seeds/SampleSeeder.php
@@ -1,5 +1,17 @@
 <?php
 
+/*
+ * This file is part of the hyn/multi-tenant package.
+ *
+ * (c) Daniël Klabbers <daniel@klabbers.email>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see https://laravel-tenancy.com
+ * @see https://github.com/hyn/multi-tenant
+ */
+
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
