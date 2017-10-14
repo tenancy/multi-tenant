80646760a0ab7dce8c70f88ad32fa6550168ca6c
luceos <daniel+github@klabbers.email>
2017-10-14 01:05:30 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Events/Hostnames/Deleting.php
+++ src/Events/Hostnames/Deleting.php
@@ -1,9 +1,21 @@
 <?php
+
+/*
+ * This file is part of the hyn/multi-tenant package.
+ *
+ * (c) Daniël Klabbers <daniel@klabbers.email>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see https://laravel-tenancy.com
+ * @see https://github.com/hyn/multi-tenant
+ */
+
 namespace Hyn\Tenancy\Events\Hostnames;
 
 use Hyn\Tenancy\Abstracts\HostnameEvent;
 
 class Deleting extends HostnameEvent
 {
-
 }
