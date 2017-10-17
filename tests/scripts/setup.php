9b1b96815796de98c029890a099444d764f2f077
Daniel Klabbers <daniel@productip.com>
2017-10-17 14:49:12 +0200

    adjusted test script to circleci

--- tests/scripts/setup.php
+++ tests/scripts/setup.php
@@ -12,21 +12,21 @@
  * @see https://github.com/hyn/multi-tenant
  */
 
+if (getenv('CIRCLE_WORKING_DIRECTORY')) {
+    putenv('CI_PROJECT_DIR=' . getenv('CIRCLE_WORKING_DIRECTORY'));
+}
+
 chdir(getenv('CI_PROJECT_DIR'));
 
 $base_path = __DIR__ . '/../../';
-
-if (getenv('TRAVIS_BUILD_DIR')) {
-    putenv('CI_PROJECT_DIR=' . getenv('TRAVIS_BUILD_DIR'));
-}
 
 /**
  * Install correct version of laravel.
  * Install db driver dependencies.
  */
 if (preg_match(
-    '/^(?<webserver>[a-z]+)\-(?<php_version>[0-9\.]+)\-(?<db>[a-z]+)$/',
-    getenv('CI_JOB_NAME'),
+    '/^php\-(?<php_version>[0-9\.]+)\-(?<webserver>[a-z]+)$/',
+    getenv('CIRCLE_JOB'),
     $m
 )) {
     putenv("BUILD_WEBSERVER={$m['webserver']}");
@@ -35,7 +35,7 @@ if (preg_match(
     echo <<<EOM
     
     
-Found advanced CI configuration from CI_JOB_NAME environment variable:
+Found advanced CI configuration from CIRCLE_JOB environment variable:
     - Webserver {$m['webserver']}
     - PHP {$m['php_version']}
     - Db driver: {$m['db']}
