ba0d741d0ac7d08ab479bb631111e4f709fe04cf
Daniël Klabbers <luceos@users.noreply.github.com>
2017-10-18 11:01:43 +0200

    Circleci (#289)

--- tests/scripts/setup.php
+++ tests/scripts/setup.php
@@ -12,43 +12,47 @@
  * @see https://github.com/hyn/multi-tenant
  */
 
+if (getenv('CIRCLE_WORKING_DIRECTORY')) {
+    putenv('CI_PROJECT_DIR=' . getenv('CIRCLE_WORKING_DIRECTORY'));
+}
+
 chdir(getenv('CI_PROJECT_DIR'));
 
 $base_path = __DIR__ . '/../../';
-
-if (getenv('TRAVIS_BUILD_DIR')) {
-    putenv('CI_PROJECT_DIR=' . getenv('TRAVIS_BUILD_DIR'));
-}
 
 /**
  * Install correct version of laravel.
  * Install db driver dependencies.
  */
 if (preg_match(
-    '/^(?<webserver>[a-z]+)\-(?<php_version>[0-9\.]+)\-(?<db>[a-z]+)$/',
-    getenv('CI_JOB_NAME'),
+    '/^php\-(?<php_version>[0-9\.]+)\-(?<webserver>[a-z]+)$/',
+    getenv('CIRCLE_JOB'),
     $m
 )) {
     putenv("BUILD_WEBSERVER={$m['webserver']}");
     putenv("BUILD_PHP_VERSION={$m['php_version']}");
+    $connection = getenv('DB_CONNECTION');
 
     echo <<<EOM
     
     
-Found advanced CI configuration from CI_JOB_NAME environment variable:
-    - Webserver {$m['webserver']}
-    - PHP {$m['php_version']}
-    - Db driver: {$m['db']}
+Found advanced CI configuration from CIRCLE_JOB environment variable:
+    - Webserver: {$m['webserver']}
+    - PHP: {$m['php_version']}
+    - Db driver: {$connection}
 
 
 EOM;
 
     foreach ([
-                 "$base_path/vendor/laravel/laravel/config/tenancy.php",
-                 "$base_path/vendor/laravel/laravel/config/webserver.php",
-             ] as $config) {
-        if (file_exists($config)) {
-            @unlink($config);
+            // Marks installation finished
+            "$base_path/vendor/laravel/laravel/tenancy.json",
+            "$base_path/vendor/laravel/laravel/config/tenancy.php",
+            "$base_path/vendor/laravel/laravel/config/webserver.php",
+     ] as $file) {
+        if (file_exists($file)) {
+            echo "Unlinked $file\n";
+            @unlink($file);
         }
     }
 }
