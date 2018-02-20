7d975ead932fd51e668bb04015c9bd10314525e2
Clark Winkelmann <clark.winkelmann@gmail.com>
2018-02-20 13:13:17 +0100

    Use trailing slash for Apache alias target (#389)
    * Use trailing slash for Apache alias target
    
    * Update server.blade.php
    
    fixed it

--- assets/generators/webserver/apache/blocks/server.blade.php
+++ assets/generators/webserver/apache/blocks/server.blade.php
@@ -8,7 +8,7 @@
 
     @if($media)
         # media directory
-        alias "/media/" "{{ $media }}"
+        alias "/media/" "{{ $media }}/"
     @endif
 
     # allow cross domain loading of resources
