55322d07dfa44dd4339dd273cbac1028180c187d
Daniel Klabbers <daniel@klabbers.email>
2018-02-21 09:25:48 +0100

    added a fallback media handler to be configured manually

---
+++ src/Controllers/MediaController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Hyn\Tenancy\Controllers;
+
+use Hyn\Tenancy\Website\Directory;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+
+/**
+ * Class MediaController
+ *
+ * @package Hyn\Tenancy\Controllers
+ * @use Route::get('/media/{path}', Hyn\Tenancy\Controllers\MediaController::class)
+ *          ->where('path', '.+')
+ *          ->name('tenant.media');
+ */
+class MediaController
+{
+    /**
+     * @var Directory
+     */
+    private $directory;
+
+    public function __construct(Directory $directory)
+    {
+        $this->directory = $directory;
+    }
+
+    public function __invoke(string $path)
+    {
+        if ($this->directory->exists($path)) {
+            return new BinaryFileResponse(
+                $this->directory->get($path)
+            );
+        }
+
+        return abort(404);
+    }
+}
