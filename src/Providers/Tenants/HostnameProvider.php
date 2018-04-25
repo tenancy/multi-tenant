57ccb3c9ab676421bb1fe3ee665caed9868f1ebc
Eric Fletcher <efletcher@paychex.com>
2018-04-25 18:21:27 -0400

    Moving CurrentHostname resolution to it's own ServiceProvider.

---
+++ src/Providers/Tenants/HostnameProvider.php
@@ -0,0 +1,28 @@
+<?php
+namespace Hyn\Tenancy\Providers\Tenants;
+
+use Hyn\Tenancy\Contracts\CurrentHostname;
+use Hyn\Tenancy\Jobs\HostnameIdentification;
+use Hyn\Tenancy\Traits\DispatchesJobs;
+use Illuminate\Support\ServiceProvider;
+
+class HostnameProvider extends ServiceProvider
+{
+    use DispatchesJobs;
+
+    public $defer = true;
+
+    public function provides()
+    {
+        return [CurrentHostname::class];
+    }
+
+    public function register()
+    {
+        $this->app->singleton(CurrentHostname::class, function () {
+            $hostname = $this->dispatch(new HostnameIdentification());
+
+            return $hostname;
+        });
+    }
+}
