4c58d1e38e6874c85308d710aaf3b3d1bec40734
Bogdan Kharchenko <32746389+bogdankharchenko@users.noreply.github.com>
2018-06-01 16:10:21 -0400

    configure host when deleting.

--- src/Generators/Webserver/Database/DatabaseGenerator.php
+++ src/Generators/Webserver/Database/DatabaseGenerator.php
@@ -136,6 +136,8 @@ class DatabaseGenerator
 
         $config = $this->connection->generateConfigurationArray($event->website);
 
+        $this->configureHost($config);
+
         $this->emitEvent(
             new Events\Database\Deleting($config, $event->website)
         );
