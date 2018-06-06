836181790d7391eaf8d1d5d85095ebedb0392a49
Mark Boychuk <boychuk.m.n@gmail.com>
2018-06-06 17:08:39 +0300

    added ability to skip request to db if this is base url

--- src/Jobs/HostnameIdentification.php
+++ src/Jobs/HostnameIdentification.php
@@ -14,9 +14,9 @@
 
 namespace Hyn\Tenancy\Jobs;
 
+use Hyn\Tenancy\Contracts\Hostname;
 use Hyn\Tenancy\Contracts\Repositories\HostnameRepository;
 use Hyn\Tenancy\Events\Hostnames\Identified;
-use Hyn\Tenancy\Contracts\Hostname;
 use Hyn\Tenancy\Traits\DispatchesEvents;
 use Illuminate\Http\Request;
 
@@ -25,16 +25,26 @@ class HostnameIdentification
     use DispatchesEvents;
 
     /**
-     * @param Request $request
+     * @param Request            $request
      * @param HostnameRepository $hostnameRepository
      * @return Hostname|null
      */
     public function handle(Request $request, HostnameRepository $hostnameRepository)
     {
-        $hostname = env('TENANCY_CURRENT_HOSTNAME');
+        $hostname       = env('TENANCY_CURRENT_HOSTNAME');
+        $check_base_url = env('TENANCY_SKIP_IF_EQUAL_TO_BASE_URL');
 
         if (!$hostname && $request->getHost()) {
             $hostname = $request->getHost();
+        }
+
+        if (
+            $check_base_url
+            && ($default_name = config('url_base'))
+            && $default_name === $hostname
+        ) {
+            $this->emitEvent(new Identified());
+            return null;
         }
 
         if ($hostname) {
