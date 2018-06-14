26ad34948162068fea5d87723aa0f2185dbaa8a5
Nitish Kumar <nitish.kumar@sugaldamani.com>
2018-06-14 09:19:33 +0530

    Catch Block Fix
    Signed-off-by: Nitish Kumar <nitish.kumar@sugaldamani.com>

--- src/Database/Console/Migrations/FreshCommand.php
+++ src/Database/Console/Migrations/FreshCommand.php
@@ -16,45 +16,45 @@ class FreshCommand extends BaseCommand
      */
     public function handle()
     {
-        if (!$this->confirmToProceed()) {
-            return;
-        }
+        try {
+            if (!$this->confirmToProceed()) {
+                return;
+            }
 
-        $this->input->setOption('force', $force = true);
-        $website_id = $this->option('website_id');
-        $realpath = $this->option('realpath');
-        $path = $this->input->getOption('path');
+            $this->input->setOption('force', $force = true);
+            $website_id = $this->option('website_id');
+            $realpath = $this->option('realpath');
+            $path = $this->input->getOption('path');
 
-        $website = $this->websites->query()->where('id', $website_id)->firstOrFail();
-        try
-        {
+            $website = $this->websites->query()->where('id', $website_id)->firstOrFail();
+
             $this->connection->set($website);
+
+            $this->dropAllTables(
+                $database = $this->connection->tenantName()
+            );
+
+            $this->info('Dropped all tables related to tenant successfully');
+
+            $this->call('tenancy:migrate', [
+                '--database' => $database,
+                '--realpath' => $realpath,
+                '--website_id' => $website_id,
+                '--path' => $path,
+                '--force' => $force,
+            ]);
+
+            if ($this->needsSeeding()) {
+                $this->call('tenancy:db:seed', [
+                    '--database' => $database,
+                    '--class' => $this->option('seeder') ?? config('tenancy.db.tenant-seed-class') ?? 'DatabaseSeeder',
+                    '--force' => $force,
+                ]);
+            }
         }
         catch (ConnectionException $e)
         {
             throw new RuntimeException(sprintf('The tenancy website_id=%d does not exist.', $website_id));
-        }
-
-        $this->dropAllTables(
-            $database = $this->connection->tenantName()
-        );
-
-        $this->info('Dropped all tables related to tenant successfully');
-
-        $this->call('tenancy:migrate', [
-            '--database' => $database,
-            '--realpath' => $realpath,
-            '--website_id' => $website_id,
-            '--path' => $path,
-            '--force' => $force,
-        ]);
-
-        if ($this->needsSeeding()) {
-            $this->call('tenancy:db:seed', [
-                '--database' => $database,
-                '--class' => $this->option('seeder') ?? config('tenancy.db.tenant-seed-class') ?? 'DatabaseSeeder',
-                '--force' => $force,
-            ]);
         }
     }
 }
