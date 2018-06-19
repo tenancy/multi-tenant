de41b28063b1c00d89830e831dbe5c092cec605f
Daniel Klabbers <daniel@klabbers.email>
2018-06-19 15:56:34 +0200

    fixed clean-local-dbs to purge users

--- tests/scripts/clean-local-dbs.sh
+++ tests/scripts/clean-local-dbs.sh
@@ -3,20 +3,21 @@
 DATABASES="$(mysql -u root -Bse 'show databases')"
 REGEX="^([a-z0-9]{8}\-?)([a-z0-9]{4}\-?){3}([a-z0-9]{12})"
 
-for db in $DATABASES; do
-    if [[ $db =~ $REGEX ]]; then
-        echo "Deleting database $db"
-        echo Y | mysqladmin -u root drop $db
+for db in ${DATABASES}; do
+    if [[ ${db} =~ ${REGEX} ]]; then
+        echo "Deleting database ${db}"
+        echo Y | mysqladmin -u root drop ${db}
     fi
 done
 
-USERS="$(mysql -u root -Bse 'select concat(User, "@", Host) from mysql.user')"
-for user in $USERS; do
-    if [[ $user =~ $REGEX ]]; then
-            echo "Deleting user $user"
-            mysql -u root -Bse "drop user $user"
+mysql -u root -D "mysql" -NBe "select User, Host from user" | while read -r user host;
+do
+    if [[ ${user} =~ ${REGEX} ]]; then
+            echo "Deleting user '${user}'@'${host}'"
+            mysql -u root -Bse "drop user '${user}'@'${host}'"
     fi
 done
+
 
 echo Y | mysqladmin -u root drop tenancy
 mysqladmin -u root create tenancy
