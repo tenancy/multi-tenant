e0d506a77340f8d960f2eb30a3c0b9cf76fd2f90
Adam Thomason <adam.thomason@hotmail.co.uk>
2018-07-19 10:11:47 +0100

    #534 - Adding TenantAwareConnection Trait (#537)
    * #534 - Adding TenantAwareConnection Trait
    
    * #534 - Removing superfluous else clause
    
    * #534 - Fix standards errors

---
+++ src/Traits/TenantAwareConnection.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the hyn/multi-tenant package.
+ *
+ * (c) Daniël Klabbers <daniel@klabbers.email>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see https://laravel-tenancy.com
+ * @see https://github.com/hyn/multi-tenant
+ */
+
+namespace Hyn\Tenancy\Traits;
+
+use Hyn\Tenancy\Database\Connection;
+
+trait TenantAwareConnection
+{
+    public function getConnectionName()
+    {
+        $hostname = app(\Hyn\Tenancy\Contracts\CurrentHostname::class);
+    
+        if ($hostname) {
+            return app(Connection::class)->tenantName();
+        }
+
+        return app(Connection::class)->systemName();
+    }
+}
