12c20fdd9434a6c20c04e11a16751406745e067a
Amirul Adli <sayaamiruladli@gmail.com>
2018-09-15 18:20:17 +0800

    Update MutatesMigrationCommands.php

--- src/Traits/MutatesMigrationCommands.php
+++ src/Traits/MutatesMigrationCommands.php
@@ -72,9 +72,98 @@ trait MutatesMigrationCommands
         }
 
         // Tenant migrations path is configured.
-        if ($path = config('tenancy.db.tenant-migrations-path')) {
-            return [$path];
+        <?php
+
+/*
+ * This file is part of the hyn/multi-tenant package.
+ *
+ * (c) Daniël Klabbers <daniel@klabbers.email>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see https://laravel-tenancy.com
+ * @see https://github.com/hyn/multi-tenant
+ */
+
+namespace Hyn\Tenancy\Traits;
+
+use Hyn\Tenancy\Contracts\Repositories\WebsiteRepository;
+use Hyn\Tenancy\Database\Connection;
+use Illuminate\Database\Migrations\Migrator;
+use InvalidArgumentException;
+
+trait MutatesMigrationCommands
+{
+    use AddWebsiteFilterOnCommand;
+    /**
+     * @var WebsiteRepository
+     */
+    private $websites;
+    /**
+     * @var Connection
+     */
+    private $connection;
+
+    public function __construct(Migrator $migrator)
+    {
+        parent::__construct($migrator);
+
+        $this->setName('tenancy:' . $this->getName());
+        $this->specifyParameters();
+
+        $this->websites = app(WebsiteRepository::class);
+        $this->connection = app(Connection::class);
+    }
+
+    public function handle()
+    {
+        if (!$this->confirmToProceed()) {
+            return;
         }
+
+        $this->input->setOption('force', true);
+        $this->input->setOption('database', $this->connection->tenantName());
+
+        $this->processHandle(function ($website) {
+            $this->connection->set($website);
+
+            parent::handle();
+
+            $this->connection->purge();
+        });
+    }
+
+    /**
+     * Get the path to the migration directory.
+     *
+     * @return string
+     */
+    protected function getMigrationPaths()
+    {
+        if ($this->input->hasOption('path') && $this->option('path')) {
+            return parent::getMigrationPaths();
+        }
+
+        // Tenant migrations path is configured.
+        if ($path = config('tenancy.db.tenant-migrations-path')) {
+            return (array) $path;
+        }
+
+        throw new InvalidArgumentException("To prevent unwanted migrations from database/migrations, always specify a path.");
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return array_merge([$this->addWebsiteOption()], parent::getOptions());
+    }
+}
+
 
         throw new InvalidArgumentException("To prevent unwanted migrations from database/migrations, always specify a path.");
     }
