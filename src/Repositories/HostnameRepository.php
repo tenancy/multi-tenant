c9d337770dd122ee623a0a6fa0e5592610278920
bastien-phi <bastien.pma.philippe@gmail.com>
2018-11-22 13:34:53 +0100

    Tweak repositories to cache null values as string (#672)
    Fixes #637 , prevent bypass of cache.

--- src/Repositories/HostnameRepository.php
+++ src/Repositories/HostnameRepository.php
@@ -61,9 +61,11 @@ class HostnameRepository implements Contract
      */
     public function findByHostname(string $hostname)
     {
-        return $this->cache->remember("tenancy.hostname.$hostname", config('tenancy.hostname.cache'), function () use ($hostname) {
-            return $this->hostname->newQuery()->where('fqdn', $hostname)->first();
+        $model = $this->cache->remember("tenancy.hostname.$hostname", config('tenancy.hostname.cache'), function () use ($hostname) {
+            return $this->hostname->newQuery()->where('fqdn', $hostname)->first() ?? 'none';
         });
+
+        return $model === 'none' ? null : $model;
     }
 
     /**
