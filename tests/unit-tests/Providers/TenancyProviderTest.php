b12c7220ef4dc72e7f9c4bec838eed9f487f3638
Vincent Prat <vpratfr@gmail.com>
2019-02-07 09:04:54 +0100

    Add test for the auto serialization of website ID for jobs

---
+++ tests/unit-tests/Providers/TenancyProviderTest.php
@@ -0,0 +1,82 @@
+<?php
+
+/*
+ * This file is part of the hyn/multi-tenant package.
+ *
+ * (c) Daniël Klabbers <daniel@klabbers.email>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see https://laravel-tenancy.com
+ * @see https://github.com/hyn/multi-tenant
+ */
+ 
+namespace Hyn\Tenancy\Tests\Providers;
+
+use Hyn\Tenancy\Tests\Test;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Queue\Events\JobProcessed;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Event;
+
+
+class TestJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function handle()
+    {
+    }
+}
+
+class TestNotification extends Notification implements ShouldQueue
+{
+    use Queueable, SerializesModels;
+
+    public function via($notifiable): array
+    {
+        return ['mail',];
+    }
+
+    public function toMail($notifiable): MailMessage
+    {
+        return new MailMessage();
+    }
+}
+
+
+class TenancyProviderTest extends Test
+{
+    /** @test */
+    public function current_website_id_is_included_in_job_payload()
+    {
+        Event::fake();
+
+        $job = new TestJob();
+        \dispatch($job);
+
+        Event::assertDispatched(JobProcessed::class, function ($event) {
+            return $event->job->payload()['website_id'] === $this->website->id;
+        });
+    }
+
+    /** @test */
+    public function current_website_id_is_included_in_notification_job_payload()
+    {
+        Event::fake();
+
+        $user = $this->userStory()->create();
+        $user->notify(new TestNotification());
+
+        Event::assertDispatched(JobProcessed::class, function ($event) {
+            return $event->job->payload()['website_id'] === $this->website->id;
+        });
+    }
+}
+
