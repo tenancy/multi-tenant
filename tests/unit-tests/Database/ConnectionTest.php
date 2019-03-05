37fd1a7dba024285a1307940f233ef30b8feea02
Daniël Klabbers <daniel@klabbers.email>
2019-03-05 14:58:03 +0100

    simply add error code 7 for pgsql for now

--- tests/unit-tests/Database/ConnectionTest.php
+++ tests/unit-tests/Database/ConnectionTest.php
@@ -138,7 +138,8 @@ class ConnectionTest extends Test
         try {
             $this->connection->get()->reconnect();
         } catch (PDOException $e) {
-            $this->assertTrue($e->getCode() == 1045, 'Access should be denied for tenant database user: [code: '.$e->getCode().'] '. $e->getMessage());
+
+            $this->assertTrue($e->getCode() === 1045 || $e->getCode() === 7, 'Access should be denied for tenant database user: [code: '.$e->getCode().'] '. $e->getMessage());
         }
 
         $this->artisan(UpdateKeyCommand::class);
