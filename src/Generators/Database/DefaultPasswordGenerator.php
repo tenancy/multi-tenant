8837ffd710d2192ce78288e9903409c35e55a442
Daniël Klabbers <daniel@klabbers.email>
2019-03-05 15:20:34 +0100

    support backward compatibility with password generator

--- src/Generators/Database/DefaultPasswordGenerator.php
+++ src/Generators/Database/DefaultPasswordGenerator.php
@@ -36,12 +36,23 @@ class DefaultPasswordGenerator implements PasswordGenerator
      */
     public function generate(Website $website) : string
     {
+        $key = $this->app['config']->get('tenancy.key');
+
+        // Backward compatibility
+        if ($key === null) {
+            return md5(sprintf(
+                '%s.%d',
+                $this->app['config']->get('app.key'),
+                $website->id
+            ));
+        }
+
         return md5(sprintf(
             '%d.%s.%s.%s',
             $website->id,
             $website->uuid,
             $website->created_at,
-            $this->app['config']->get('tenancy.key')
+            $key
         ));
     }
 }
