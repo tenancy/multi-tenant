53835065ff8332b32917efd596818bc56bbcbbc4
ArlonAntonius <37894143+ArlonAntonius@users.noreply.github.com>
2019-10-26 14:41:30 +0200

    Return queue as well

--- src/Providers/Tenants/QueueProvider.php
+++ src/Providers/Tenants/QueueProvider.php
@@ -37,6 +37,7 @@ class QueueProvider extends ServiceProvider
 
                 return ['website_id' => $website_id];
             });
+            return $queue;
         });
 
         $this->app['events']->listen(JobProcessing::class, function ($event) {
